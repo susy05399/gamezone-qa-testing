bee260b0ab94970afa15c570d6fb2ed2f44bc92d
Susana Pérez Aparicio <perezapariciosusana@gmail.com>
2025-05-05 23:29:19 -0600

    Update conftest.py

--- automated_testing/conftest.py
+++ automated_testing/conftest.py
@@ -1,1 +1,13 @@
+# automated_testing/conftest.py
 
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
